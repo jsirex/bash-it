4cfd2f94013e80449fd0498b75da1975744d47c9
Yauhen Artsiukhou <jsirex@gmail.com>
2015-04-20 13:44:23 +0300

    adding apt aliases

---
+++ aliases/available/apt.aliases.bash
@@ -0,0 +1,9 @@
+cite 'about-alias'
+about-alias 'apt-get and aptitude aliases'
+
+# Aliases
+alias a-u='sudo aptitude update'
+alias a-d='sudo aptitude dist-upgrade'
+alias aud='sudo aptitude update; sudo aptitude dist-upgrade'
+alias spkg='apt-cache search'
+
