1eadf66c1414e4905eb7e136ff64ecbf2d9c8a11
Yauhen Artsiukhou <yauhen_artsiukhou@epam.com>
2015-04-22 11:33:25 +0300

    tweak history length and saving

--- lib/history.bash
+++ lib/history.bash
@@ -11,7 +11,8 @@ export HISTCONTROL="ignoredups"
 export HISTCONTROL=erasedups
 
 # resize history size
-export HISTSIZE=5000
+export HISTSIZE=500000
+export HISTFILESIZE=1000000
 
 export AUTOFEATURE=true autotest
 
