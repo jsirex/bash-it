979425e35693140b59f9dae38e0fee825ef22f40
Yauhen Artsiukhou <jsirex@gmail.com>
2015-05-07 01:03:22 +0300

    added alias ai: aptitude install

--- aliases/available/apt.aliases.bash
+++ aliases/available/apt.aliases.bash
@@ -5,5 +5,6 @@ about-alias 'apt-get and aptitude aliases'
 alias a-u='sudo aptitude update'
 alias a-d='sudo aptitude dist-upgrade'
 alias aud='sudo aptitude update; sudo aptitude dist-upgrade'
+alias ai='sudo aptitude install'
 alias spkg='apt-cache search'
 
