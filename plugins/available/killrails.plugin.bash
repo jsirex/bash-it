a8f07cb7e3ebb4896da3f0cd48b52bb451edd6ed
Nils Winkler <nils@nilswinkler.com>
2016-05-19 09:03:01 +0200

    Fixed indentation
    It was using a mixture of tabs and spaces. Converted to 2 spaces.

--- plugins/available/killrails.plugin.bash
+++ plugins/available/killrails.plugin.bash
@@ -9,10 +9,10 @@ function killrails() {
   railsPid="$(cat tmp/pids/server.pid)"
   if [ ! -z "$railsPid" ]; then
     echo "[OK] Rails Server Process Id : ${railsPid}"
-	kill -9 $railsPid
-	echo "[OK] Process Killed"
+    kill -9 $railsPid
+    echo "[OK] Process Killed"
   else
-	echo "[FAIL] Error killing Rails server"
-	return 1
+    echo "[FAIL] Error killing Rails server"
+    return 1
   fi
 }
